82735c54731efc47f49eb9507e57aba3590e8ee2
Ramya Gangula <ramya1331@gmail.com>
2019-04-09 23:50:55 -0500

    adding logic for collection creation

--- src/main/java/com/ms/consoleapp/MongoApiTest.java
+++ src/main/java/com/ms/consoleapp/MongoApiTest.java
@@ -1,7 +1,7 @@
 package com.ms.consoleapp;
 
 import com.mongodb.reactivestreams.client.*;
-import org.bson.Document;
+import org.bson.*;
 
 import static java.util.Arrays.asList;
 
@@ -10,10 +10,12 @@ public class MongoApiTest {
     public static void main(String[] args) throws Throwable {
 
 
-        String connectionString = "mongodb://mongoapipr:mYfGaSXkDTlnxwv6YKyXVLP5LVdrGVysTCtytVddgKFcSJD14w83QGSlFvC1xrJQX9dqBmXl2sHyypg9TRx0IA==@mongoapipr.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+        String connectionString = "mongodb://mongoapipr:Sopcfk46JcrNIiTfd0Vj6YdbdcA5L8Fq6RzJpu24VJxbE1nfyure2ysjOv23sRtukRf7D5FeXZaI4Gw0AvLkqQ==@mongoapipr.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
         String databaseName = "testdb";
         String collectionName = "testcol";
+        String partitionKey = "Country";
 
+        // create Connection
         MongoClient mongoClient = MongoClients.create(connectionString);
 
         // getting a list of databases and printing them using PrintStatus Subscriber
@@ -21,6 +23,27 @@ public class MongoApiTest {
 
         // get a handle of the database
         MongoDatabase database = mongoClient.getDatabase(databaseName);
+
+
+        ObservableSubscriber<Document> obsSubscriber = new ObservableSubscriber<Document>();
+
+        // create new partitioned collection
+        int rus = 800;
+        Document col1 = new Document("customAction", "CreateCollection")
+                .append("collection", collectionName).append("offerThroughput", rus)
+                .append("shardKey",partitionKey);
+        database.runCommand(col1).subscribe(obsSubscriber);
+        // ignoring below await will  create fixed collection :(
+        obsSubscriber.await();
+
+        // updated collection RU's
+        int updatedRus = 1200;
+        obsSubscriber = new ObservableSubscriber<Document>();
+        Document cmd2 = new Document("customAction", "UpdateCollection").
+                append("collection", collectionName).append("offerThroughput", updatedRus);
+        database.runCommand(cmd2).subscribe(obsSubscriber);
+        obsSubscriber.await();
+
 
         // getting a list of collections and printing them using PrintStatus Subscriber
         database.listCollectionNames().subscribe(new PrintStatus<String>("Collection Names: %s"));
@@ -43,9 +66,9 @@ public class MongoApiTest {
 
 
         // drop collection
-        subscriber = new ObservableSubscriber<Success>();
-        collection.drop().subscribe(subscriber);
-        subscriber.await();
+//        subscriber = new ObservableSubscriber<Success>();
+//        collection.drop().subscribe(subscriber);
+//        subscriber.await();
 
 
         mongoClient.close();
